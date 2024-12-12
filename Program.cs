1150dcf97c74099627b7a2e6328569171b278356
Олег Мамонтов <rus.lapshim765@gmail.com>
2024-12-12 17:45:42 +0300

    Создание переменых

--- Program.cs
+++ Program.cs
@@ -10,6 +10,20 @@ namespace dumpling_factory
     {
         static void Main(string[] args)
         {
+            double productionLineProductivityPelmenny;
+            double productionLineProductivityTesta;
+            double productionLineProductivityFarsha;
+            double machinePerformancePelmenny;
+            double machinePerformanceTesta;
+            double machinePerformanceFarsha;
+
+            double dailyOutput; // суточная выработка готовой продукции 
+            int shiftTime; // продолжительность рабочей смены
+            double massFractionOfDough; //массовая доля теста 
+            double massFractionOfMincedMeat; //массовая доля фарша
+
+
+
         }
     }
 }
